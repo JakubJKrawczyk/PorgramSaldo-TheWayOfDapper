be8d25da3c2890eac8ba92aef985ff9636112ce5
Lakasabasz <lakasabasz@gmail.com>
2022-08-02 12:16:39 +0200

    Resolves #16

---
+++ DapperDynamic/docs/html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['dapperdynamic_0',['DapperDynamic',['../namespace_dapper_dynamic.html',1,'']]],
+  ['databasemanager_1',['DatabaseManager',['../class_database_manager.html',1,'']]]
+];
---
+++ DapperDynamic/docs/html/search/namespaces_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dapperdynamic_0',['DapperDynamic',['../namespace_dapper_dynamic.html',1,'']]]
+];
